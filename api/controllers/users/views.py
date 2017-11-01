d0d22d6ecf0f03f07d3859d012a4185e6029ccfb
Pavel <grigorevpv@gmail.com>
2017-11-02 00:23:16 +0300

    rewrite /api/user/<nickname>/create and /api/user/<nickname>/profile

--- api/controllers/users/views.py
+++ api/controllers/users/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, make_response, jsonify
 from api.services.UserService.UserService import UserService
+from api.repositories.UserRepository.user_queries_db import *
 from api.models.users.UserModel import UserModel
+from api.repositories.connect import connectDB
 
 # create new blueprint
 users_blueprint = Blueprint('users', 'users', url_prefix='/api/user')
@@ -19,38 +21,33 @@ STATUS_CODE = {
 def create_user(nickname):
 	content = request.get_json(silent=True)
 	content['nickname'] = nickname
-	user = user_model.from_dict(content)
+	connect = connectDB()
+	cursor = connect.cursor()
 
-	user, code = user_service.create_user(user)
-
-	if code == STATUS_CODE['CREATED']:
-		param_name_array = ["nickname", "about", "email", "fullname"]
-		param_value_array = [user.nickname, user.about, user.email, user.fullname]
-		user = dict(zip(param_name_array, param_value_array))
-
-		return make_response(jsonify(user), code)
-
-	if code == STATUS_CODE['CONFLICT']:
-
-		return make_response(jsonify(user), code)
+	try:
+		cursor.execute(INSERT_USER, [content['nickname'], content['about'], content['email'], content['fullname'], ])
+		return make_response(jsonify(content), STATUS_CODE['CREATED'])
+	except:
+		users = []
+		cursor.execute(SELECT_USERS_BY_NICKNAME_OR_EMAIL, [content['nickname'], content['email'], ])
+		for user in cursor.fetchall():
+			users.append(dict(zip(['nickname', 'about', 'email', 'fullname'], user[1:])))
+		return make_response(jsonify(users), STATUS_CODE['CONFLICT'])
 
 
 @users_blueprint.route('/<nickname>/profile', methods=['GET'])
 def get_user_profile(nickname):
+	connect = connectDB()
+	cursor = connect.cursor()
+
+	cursor.execute(SELECT_USERS_BY_NICKNAME, [nickname, ])
+	user = cursor.fetchone()
+	if user is None:
+		return make_response(jsonify({"message": "Can't find user with nickname: " + nickname}), STATUS_CODE['NOT_FOUND'])
+
 	content = dict()
-
-	message_or_user, code = user_service.select_user_by_nickname(nickname)
-
-	if code == STATUS_CODE['OK']:
-		param_name_array = ["nickname", "about", "email", "fullname"]
-		param_value_array = [message_or_user.nickname, message_or_user.about, message_or_user.email, message_or_user.fullname]
-		message_or_user = dict(zip(param_name_array, param_value_array))
-
-		return make_response(jsonify(message_or_user), code)
-
-	if code == STATUS_CODE['NOT_FOUND']:
-
-		return make_response(jsonify(message_or_user), code)
+	content['nickname'], content['about'], content['email'], content['fullname'] = user[1:]
+	return make_response(jsonify(content), STATUS_CODE['OK'])
 
 
 @users_blueprint.route('/<nickname>/profile', methods=['POST'])
