c024a580a8acf847e943cd7a7103a53381894b3e
Pavel <grigorevpv@gmail.com>
2017-11-07 16:35:50 +0300

    fix thread/<slug_or_id>/vote

--- api/controllers/threads/views.py
+++ api/controllers/threads/views.py
@@ -13,6 +13,7 @@ from api.models.posts.PostModel import PostModel
 from api.repositories.connect import PostgresDataContext
 from api.repositories.ThreadRepository.thread_queries_db import *
 from api.repositories.PostRepository.post_queries_db import *
+from api.repositories.VoteRepository.vote_queries_db import *
 from enquiry.queries_db import *
 from enquiry.connect import *
 from enquiry.secondary import *
@@ -81,15 +82,6 @@ def create_posts(slug_or_id):
 			return make_response(jsonify({"message": "Can't find forum with forum_id: " + thread["forum_id"]}),
 								 STATUS_CODE['NOT_FOUND'])
 
-		# if post.get("slug") is not None:
-		# 	cursor.execute( SELECT_THREAD_BY_SLUG, [post["slug"], ] )
-		# 	returning_thread = cursor.fetchone()
-		# 	if returning_thread is not None:
-		# 		data_context.put_connection( connect )
-		# 		cursor.close()
-		# 		return make_response( jsonify( {"message": "This thread is exist slug: " + post["slug"]} ),
-		# 		                      STATUS_CODE[ 'CONFLICT' ] )
-
 		post_path = list()
 		parent_id = 0
 		if post.get('parent') is not None:
@@ -126,103 +118,98 @@ def create_posts(slug_or_id):
 	return make_response(jsonify(created_threads_arr), STATUS_CODE['CREATED'])
 
 
-
-
-
-
-
-
-
-
-
-	# message_or_thread, code = thread_service.select_thread_by_slug_or_id(slug_or_id)
-	#
-	# if code == STATUS_CODE['OK']:
-	# 	created_threads_arr = []
-	# 	created_time = datetime.now()
-	#
-	# 	for post in content:
-	# 		user, status_code = user_service.select_user_by_nickname(post['author'])
-	# 		if status_code == STATUS_CODE['NOT_FOUND']:
-	# 			return make_response(jsonify(user), status_code)
-	# 		forum, status_code = forum_service.select_forum_by_id(message_or_thread.forum_id)
-	#
-	# 		path = list()
-	# 		post_content = dict()
-	# 		post_content['user_id'] = user.id
-	# 		post_content['thread_id'] = message_or_thread.id
-	# 		post_content['forum_id'] = message_or_thread.forum_id
-	# 		post_content['created'] = created_time
-	# 		post_content['message'] = post['message']
-	# 		if post.get('parent') is not None:
-	# 			try:
-	# 				message_or_parent_post, status_code = post_service.select_post_by_id(post['parent'])
-	# 				if message_or_parent_post.thread_id == message_or_thread.id:
-	# 					post_content['parent_id'] = post['parent']
-	# 				else:
-	# 					return make_response(jsonify({"message": "Parent post was created in another thread"}), STATUS_CODE['CONFLICT'])
-	# 			except:
-	# 				return make_response(jsonify({"message": "Cant't find parent post"}), STATUS_CODE['CONFLICT'])
-	# 		else:
-	# 			post_content['parent_id'] = 0
-	# 		post_content['path'] = path
-	# 		post = post_model.from_dict(post_content)
-	#
-	# 		created_post, status_code = post_service.create_post(post)
-	#
-	# 		param_name_array = ["author", "created", "forum", "id", "isEdited", "message",
-	# 							"parent", "thread"]
-	# 		param_value_array = [user.nickname, convert_time(created_post.created), forum.slug, created_post.id, created_post.isedited,
-	# 		                     created_post.message, created_post.parent_id, message_or_thread.id]
-	# 		created_thread_data = dict(zip(param_name_array, param_value_array))
-	# 		created_threads_arr.append(created_thread_data)
-	#
-	# 	return make_response(jsonify(created_threads_arr), STATUS_CODE['CREATED'])
-	# if code == STATUS_CODE['NOT_FOUND']:
-	#
-	# 	return make_response(jsonify(message_or_thread), code)
-
-
 @threads_blueprint.route('/<slug_or_id>/vote', methods=['POST'])
 def create_vote(slug_or_id):
 	content = request.get_json(silent=True)
-	user_content = dict()
-	user_content['nickname'] = content['nickname']
-	user = user_model.from_dict(user_content)
-	vote = vote_model.from_dict(content)
+	connect, cursor = data_context.create_connection()
 
-	message_or_thread, code = thread_service.select_thread_by_slug_or_id(slug_or_id)
+	if slug_or_id.isdigit():
+		cursor.execute(SELECT_THREAD_BY_ID, [slug_or_id, ])
+		thread = cursor.fetchone()
+		if thread is None:
+			data_context.put_connection(connect)
+			cursor.close()
+			return make_response(jsonify({"message": "Can't find thread with id: " + slug_or_id}),
+								 STATUS_CODE['NOT_FOUND'])
+	else:
+		cursor.execute(SELECT_THREAD_BY_SLUG, [slug_or_id, ])
+		thread = cursor.fetchone()
+		if thread is None:
+			data_context.put_connection(connect)
+			cursor.close()
+			return make_response(jsonify({"message": "Can't find thread with id: " + slug_or_id}),
+								 STATUS_CODE['NOT_FOUND'])
 
-	if code == STATUS_CODE['OK']:
-		thread, user, message_or_vote, code = vote_service.create_vote(message_or_thread, user, vote)
+	cursor.execute(SELECT_USERS_BY_NICKNAME, [content['nickname']])
+	user = cursor.fetchone()
+	if user is None:
+		data_context.put_connection(connect)
+		cursor.close()
+		return make_response(jsonify({"message": "Can't find user with nickname: " + post['author']}),
+		                     STATUS_CODE['NOT_FOUND'])
 
-		if code == STATUS_CODE['NOT_FOUND']:
-			return make_response(jsonify(message_or_vote), code)
-		if code == STATUS_CODE['OK']:
-			forum, status_code = forum_service.select_forum_by_id(thread.forum_id)
-			count_votes, status_code = vote_service.count_votes_by_thread_id(thread.id)
-			message_or_user, status_code = user_service.select_user_by_user_id(thread.user_id)
-			if status_code == STATUS_CODE['NOT_FOUND']:
-				return make_response(jsonify(message_or_user), status_code)
+	cursor.execute(SELECT_VOTE_BY_THREAD_AND_USER_ID, [thread["id"], user["user_id"]])
+	vote = cursor.fetchone()
+	if vote is None:
+		cursor.execute(INSERT_VOTE, [user["user_id"], thread["id"], content["voice"], ])
+		vote = cursor.fetchone()
+	else:
+		if vote["voice"] != content["voice"]:
+			cursor.execute(UPDATE_VOTE, [content["voice"], user["user_id"], thread["id"], ])
+			vote = cursor.fetchone()
 
-			if thread.created is not None:
-				param_name_array = ["author", "created", "forum", "id", "message", "slug", "title", "votes"]
-				param_value_array = [message_or_user.nickname, convert_time(thread.created),
-				                     forum.slug, thread.id, thread.message,
-				                     thread.slug, thread.title, count_votes]
-			else:
-				param_name_array = ["author", "forum", "id", "message", "slug", "title", "votes"]
-				param_value_array = [message_or_user.nickname, forum.slug,
-				                     message_or_thread.id, message_or_thread.message,
-				                     message_or_thread.slug, message_or_thread.title, count_votes]
+	cursor.execute(COUNT_VOTES_BY_THREAD_ID, [thread["id"], ])
+	count_votes = cursor.fetchone()["votes_count"]
 
-			thread_data = dict(zip(param_name_array, param_value_array))
+	thread["votes"] = count_votes
+	thread["created"] = convert_time(thread["created"])
 
-			return make_response(jsonify(thread_data), code)
+	data_context.put_connection(connect)
+	cursor.close()
+	return make_response(jsonify(thread), STATUS_CODE['OK'])
 
-	if code == STATUS_CODE['NOT_FOUND']:
 
-		return make_response(jsonify(message_or_thread), code)
+# @threads_blueprint.route('/<slug_or_id>/vote', methods=['POST'])
+# def create_vote(slug_or_id):
+# 	content = request.get_json(silent=True)
+# 	user_content = dict()
+# 	user_content['nickname'] = content['nickname']
+# 	user = user_model.from_dict(user_content)
+# 	vote = vote_model.from_dict(content)
+#
+# 	message_or_thread, code = thread_service.select_thread_by_slug_or_id(slug_or_id)
+#
+# 	if code == STATUS_CODE['OK']:
+# 		thread, user, message_or_vote, code = vote_service.create_vote(message_or_thread, user, vote)
+#
+# 		if code == STATUS_CODE['NOT_FOUND']:
+# 			return make_response(jsonify(message_or_vote), code)
+# 		if code == STATUS_CODE['OK']:
+# 			forum, status_code = forum_service.select_forum_by_id(thread.forum_id)
+# 			count_votes, status_code = vote_service.count_votes_by_thread_id(thread.id)
+# 			message_or_user, status_code = user_service.select_user_by_user_id(thread.user_id)
+# 			if status_code == STATUS_CODE['NOT_FOUND']:
+# 				return make_response(jsonify(message_or_user), status_code)
+#
+# 			if thread.created is not None:
+# 				param_name_array = ["author", "created", "forum", "id", "message", "slug", "title", "votes"]
+# 				param_value_array = [message_or_user.nickname, convert_time(thread.created),
+# 				                     forum.slug, thread.id, thread.message,
+# 				                     thread.slug, thread.title, count_votes]
+# 			else:
+# 				param_name_array = ["author", "forum", "id", "message", "slug", "title", "votes"]
+# 				param_value_array = [message_or_user.nickname, forum.slug,
+# 				                     message_or_thread.id, message_or_thread.message,
+# 				                     message_or_thread.slug, message_or_thread.title, count_votes]
+#
+# 			thread_data = dict(zip(param_name_array, param_value_array))
+#
+# 			return make_response(jsonify(thread_data), code)
+#
+# 	if code == STATUS_CODE['NOT_FOUND']:
+#
+# 		return make_response(jsonify(message_or_thread), code)
+
 
 @threads_blueprint.route('/<slug_or_id>/details', methods=['GET'])
 def get_thread_information(slug_or_id):
--- api/repositories/ThreadRepository/thread_queries_db.py
+++ api/repositories/ThreadRepository/thread_queries_db.py
@@ -5,12 +5,18 @@ SELECT_THREADS_BY_FORUM_ID = '''SELECT threads.id, threads.forum_id, threads.use
 COUNT_VOTES_BY_THREAD_ID = '''SELECT sum(voice) as votes_count FROM votes 
 								WHERE thread_id = %s;'''
 
-SELECT_THREAD_BY_ID = '''SELECT threads.id, threads.forum_id, threads.user_id, 
-							threads.created, threads.message, threads.slug, threads.title 
+# SELECT_THREAD_BY_ID = '''SELECT threads.id, threads.forum_id, threads.user_id,
+# 							threads.created, threads.message, threads.slug, threads.title
+# 							FROM threads WHERE id = %s;'''
+
+# SELECT_THREAD_BY_SLUG = '''SELECT threads.id, threads.forum_id, threads.user_id,
+# 							threads.created, threads.message, threads.slug, threads.title
+# 							FROM threads WHERE slug = %s;'''
+
+SELECT_THREAD_BY_ID = '''SELECT *
 							FROM threads WHERE id = %s;'''
 
-SELECT_THREAD_BY_SLUG = '''SELECT threads.id, threads.forum_id, threads.user_id, 
-							threads.created, threads.message, threads.slug, threads.title 
+SELECT_THREAD_BY_SLUG = '''SELECT * 
 							FROM threads WHERE slug = %s;'''
 
 UPDATE_THREAD = '''UPDATE threads SET threads.message = %s, threads.title = %s
